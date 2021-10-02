c05f6553504c8748035320c95e075d44ff1c3ab2
tatu_heinamaki <tatu.heinamaki@gmail.com>
2021-10-02 14:41:47 +0300

    Heart manager.

---
+++ Assets/Scripts/HeartManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Reflects the heart status
+// Script that manages the heartbeat animation and audio
+public class HeartManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
