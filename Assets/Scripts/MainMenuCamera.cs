aa941d566939ffee0dd18b62ae5a78017bf7fe13
Aleksi O. Martikainen <aleksi.o.martikainen@student.tut.fi>
2021-10-04 17:08:37 +0300

    fix mainmenu after win

--- Assets/Scripts/MainMenuCamera.cs
+++ Assets/Scripts/MainMenuCamera.cs
@@ -9,11 +9,14 @@ public class MainMenuCamera : MonoBehaviour
     private void Start()
     {
         table = GameObject.Find("table");
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
     }
 
     void Update()
     {
         transform.LookAt(table.transform);
         transform.Translate(Vector3.right * Time.deltaTime * .1f);
+        //Cursor.visible = true;
     }
 }
